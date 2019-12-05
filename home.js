648ba71df8cd6c6b42d032d42d393fd3abeef5ab
Chandra Bhushan Sharma <chandrabhu24@gmail.com>
2019-12-05 10:18:53 +0530

    Update home.js

--- home.js
+++ home.js
@@ -20,18 +20,22 @@ Message :<br>
 <p> {{ message }} </p>
 </div>
 
-
-
-
-
-
-
 //Using Controller
 <table ng-controller="BasketCtrl">
 <tr><td>Item:</td><td>{{ description }}</td></tr>
 <tr><td>Cost:</td><td>${{ cost }}</td></tr>
 <tr><td>Qty: </td><td><input type="number" ng-model="qty" ></td></tr>
 <tr><td>Subtotal:</td><td>{{qty * cost | currency}} </td></tr>
+</table>
+
+//Get External Data
+<table ng-controller="TimetableCtrl">
+<tr><th>time</th><th>title</th><th>detail </th></tr>
+<tr ng-repeat="session in sessions">
+<td>{{ session . time }} </td>
+<td>{{ session.title }}</td>
+<td>{{ session.detail }}</td>
+</tr>
 </table>
 
 
