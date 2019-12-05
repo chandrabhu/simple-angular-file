ad9b955783fad0381059c53e2c562a8bba31b725
Chandra Bhushan Sharma <chandrabhu24@gmail.com>
2019-12-05 10:09:56 +0530

    Update home.js

--- home.js
+++ home.js
@@ -3,23 +3,39 @@
 <head>
 
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.2/angular.min.js"></script>
+<script src="./angular-controller.js"></script>
 </head>
 
-
 <body>
-        
+//Simple
 <form>
 To :<br>
-<input ng-model =" name" type="text"/><br>
+<input ng-model="name" type="text"/><br>
 Message :<br>
-<textarea ng-model ="message"></textarea>
+<textarea ng-model="message"></textarea>
 <input type="submit" value="send message" />
 </form>
-
-
 <div class="postcard">
 <div>{{ name }} </div>
 <p> {{ message }} </p>
 </div>
+
+
+
+
+
+
+
+//Using Controller
+<table ng-controller="BasketCtrl">
+<tr><td>Item:</td><td>{{ description }}</td></tr>
+<tr><td>Cost:</td><td>${{ cost }}</td></tr>
+<tr><td>Qty: </td><td><input type="number" ng-model="qty" ></td></tr>
+<tr><td>Subtotal:</td><td>{{qty * cost | currency}} </td></tr>
+</table>
+
+
+
+
 </body>
 </html>
