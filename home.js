7ac470994d239943f736b21a085d4706bd9d424d
Chandra Bhushan Sharma <chandrabhu24@gmail.com>
2019-12-05 09:44:32 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
-<html ng- app>
-<head> ...
-c09/ angular-introducti on.html
-<script src="https://aj ax.googleapi s . com/ajax/
-l i bs/angularjs/1 .0.2/angular.min.js"></scri pt>
+<html ng-app>
+<head>
+
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.2/angular.min.js"></script>
 </head>
-<body> ...
+
+
+<body>
+        
 <form>
 To :<br>
 <input ng-model =" name" type="text"/><br>
 Message :<br>
 <textarea ng-model ="message"></textarea>
 <input type="submit" value="send message" />
-</form> ...
+</form>
+
+
 <div class="postcard">
 <div>{{ name }} </div>
 <p> {{ message }} </p>
