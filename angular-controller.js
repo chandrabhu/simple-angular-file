9bc3caf9a840d56223260f8aca0f6694154f411a
Chandra Bhushan Sharma <chandrabhu24@gmail.com>
2019-12-05 10:19:11 +0530

    Update angular-controller.js

--- angular-controller.js
+++ angular-controller.js
@@ -2,4 +2,10 @@ function BasketCtrl ($scope){
 $scope.description = 'Single';
 $scope.cost = 8;
 $scope.qty = 1;
- }
+}
+
+ function TimetableCtrl ($scope, $http) {
+ $http.get('js/items.json')
+.success(function(data) { $scope.sessions = data.sessions; })
+.error(function(data) { console.log(' error') }) ;
+}
